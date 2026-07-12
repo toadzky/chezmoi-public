203ca2d0866a3c04b206d23b18d29a6641bae22e
David Harris <toadzky@gmail.com>
2026-07-12 17:40:55 -0600

    fixes bash script

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,13 +24,13 @@ lock-after=false
     echo "Skeleton 'login' keyring injected. It will prompt for a password upon next application access."
 fi
 
-export PATH="~/.local/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 if command -v dcli; then
     echo "Dashlane CLI already installed, proceeding..."
 else
     mkdir -p ~/.local/bin
     DOWNLOAD_URL=$(curl -s https://api.github.com/repos/Dashlane/dashlane-cli/releases/latest | jq '.assets[] | select(.name | contains("linux-x64")) | .browser_download_url' -r)
-    curl -L -o "~/.local/bin/dcli" "$DOWNLOAD_URL"
+    curl -L -o "$HOME/.local/bin/dcli" "$DOWNLOAD_URL"
     chmod +x ~/.local/bin/dcli
 fi
 
@@ -44,7 +44,7 @@ chmod 600 "$HOME/.config/chezmoi/key.txt"
 
 TOKEN=$(dcli note chezmoi -o json | jq '.[0].content | fromjson | .githubToken' -r)
 echo "Installing chezmoi..."
-sh -c "$(curl -fsLS https://chezmoi.io/lb)"
+sh -c "$(curl -fsLS https://get.chezmoi.io/lb)"
 
 echo "Initilizing and applying chezmoi configuration..."
 chezmoi init --apply "https://${TOKEN}@github.com/toadzky/chezmoi-private"
